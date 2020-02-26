b878663f547923f30694c7130b765c116df69515
HlebK20 <61520389+HlebK20@users.noreply.github.com>
2020-02-27 01:12:57 +0300

    Working program

---
+++ DEV-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DEV_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string string_from_cmd;
+            string_from_cmd = Console.ReadLine();
+
+            int count = 1;
+            int maxcount = count;
+
+            for (int i = 1; i < string_from_cmd.Length; i++)
+            {
+                if (string_from_cmd[i] == string_from_cmd[i - 1])
+                {
+                    count++;
+                }
+                if (maxcount < count)
+                {
+                    maxcount = count;
+                }
+                if (string_from_cmd[i] != string_from_cmd[i - 1])
+                {
+                    count = 1;
+                }
+            }
+            Console.WriteLine(maxcount);
+        }
+    }
+}
