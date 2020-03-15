b6e3e27b142a4a37f90c7a0f563ddac78bdc5958
HlebK20 <61520389+HlebK20@users.noreply.github.com>
2020-03-15 23:29:27 +0300

    Revorked task_DEV-1 and added tests

---
+++ DEV-1/StringDif.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEV_1
+{
+    public class StringDif
+    {
+        public static int CountDifferentSymbols(string string_from_cmd)
+        {
+            if (string_from_cmd.Length == 0)
+                return 0;
+            int count = 1;
+            int maxcount = count;
+            for (int i = 1; i < string_from_cmd.Length; i++)
+            {
+
+                if (string_from_cmd[i] == string_from_cmd[i - 1])
+                {
+                    count = 1;
+                }
+                if (string_from_cmd[i] != string_from_cmd[i - 1])
+                {
+                    count++;
+                }
+                if (maxcount < count)
+                {
+                    maxcount = count;
+                }
+            }
+            return maxcount;
+        }
+    }
+}
---
+++ DEV-1Tests/StringDifTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DEV_1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEV_1.Tests
+{
+    [TestClass()]
+    public class StringDifTests
+    {
+        [TestMethod()]
+        public void StringDiffCountDifferentSymbols_CountingCorrectly()
+        {
+            string str1 = "hello world";
+            int actual = StringDif.CountDifferentSymbols(str1);
+            int expected = 8;
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod()]
+        public void StringDiffCountDifferentSymbols_OneSymbol()
+        {
+            string str1 = "H";
+            int actual = StringDif.CountDifferentSymbols(str1);
+            int expected = 1;
+            Assert.AreEqual(actual,expected);
+        }
+        [TestMethod()]
+        public void StringDiffCountDifferentSymbols_EmptyString()
+        {
+            string str1 = "";
+            int actual = StringDif.CountDifferentSymbols(str1);
+            int expected = 0;
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(NullReferenceException),"String is null")]
+        public void StringDiffCountDifferentSymbols_NullString()
+        {
+            string str1 = null;
+            StringDif.CountDifferentSymbols(str1);
+        }
+        [TestMethod()]
+        public void StringDiffCountDifferentSymbols_VeryLargeString()
+        {
+            string str1 = "start";
+            while (1073741823 / str1.Length > 1)
+            {
+                str1 += str1;
+            }
+            int actual= StringDif.CountDifferentSymbols(str1);
+            int expected = str1.Length ;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
