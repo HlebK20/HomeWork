eae6b497b6913746899ee10249c45faff47ba53d
Hleb Kokorev <61520389+HlebK20@users.noreply.github.com>
2020-03-26 21:09:03 +0300

    Added work from cmd

--- task_DEV-1/DEV-1/Program.cs
+++ task_DEV-1/DEV-1/Program.cs
@@ -4,9 +4,16 @@ namespace DEV_1
 {
     class Program
     {
-        static void Main()
+       static void Main(string []args)
         {
-
-        }
+            try
+            {
+               string str = args[0];
+               Console.WriteLine("DifferentSymbolMaxLengthCounter.CountDifferentSymbols(str)");
+            }
+            catch (Exception Exc)
+            {
+                Console.WriteLine(Exc.Message);
+            }
     }
 }
