9df26ea622970a5a4814c8d2715c5068ac55532b
HlebK20 <61520389+HlebK20@users.noreply.github.com>
2020-03-31 19:29:49 +0300

    Added launch from cmd

--- task_DEV-5/task_DEV-5/Program.cs
+++ task_DEV-5/task_DEV-5/Program.cs
@@ -6,7 +6,17 @@ namespace task_DEV_5
     {
         static void Main(string[] args)
         {
-           
+            try
+            {
+                string str = args[0];
+                Console.WriteLine(str.CountDifferentSymbolsMaxLength());
+                Console.WriteLine(str.CountSameLettersMaxLength());
+                Console.WriteLine(str.CountSameNumbersMaxLength());
+            }
+            catch (Exception Exc)
+            {
+                Console.WriteLine(Exc.Message);
+            }
         }
     }
 }
